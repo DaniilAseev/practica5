a0d9b5b065ca850c46e9954487312fa4ddf10541
Denis <denisjutov@gmail.com>
2024-12-24 19:57:24 +0700

    Реализована функция (x)

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from functions import sin_series, sh_series
+from functions import sin_series, sh_series, arctan_series
 
 def main():
     print("Выберите функцию:")
     print("1. sin(x)")
     print("2. sh(x)")
+    print("3. arctan(x)")
     choice = int(input("Введите номер функции: "))
     x = float(input("Введите значение x: "))
 
@@ -13,8 +14,11 @@ def main():
     elif choice == 2:
         result = sh_series(x)
         print(f"sh({x}) = {result}")
+    elif choice == 3:
+        result = arctan_series(x)
+        print(f"arctan({x}) = {result}")
     else:
         print("Неверный выбор.")
 
-if __name__ == "__main__":
+if __name__== "__main__":
     main()
